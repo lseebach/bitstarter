f8c7f0ed9c6ff84acca62e22e25b5b4c60052306
Lars Seebach <lars@seebach.me>
2013-07-05 13:49:49 +0000

    changed

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-
+var file = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  file = fs.readFileSync('index.html');
-  response.send(file);
+  	// write HTTP-HEAD
+	response.writeHead(200);
+	response.write(file,"binary");
+	response.end();
 
 });
 
