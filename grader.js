5a03f6f8b39a7c15c3dfa5caa3b15b6d886aac9e
Lars Seebach <lars@seebach.me>
2013-07-08 12:02:04 +0200

    changed grader to load HTML via restler asynchroniously added sample test data

--- grader.js
+++ grader.js
@@ -19,13 +19,23 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
++ restler
+   - https://github.com/danwrong/restler
+
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+
+
+//Defaults
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://bitstarter-lse.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,24 +46,82 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtml = function(html) {
+    console.log("entering cheerioHtml");
+    return cheerio.load(html);
+};
+
+var restlerHtmlFile = function(url, checksfile) {
+    console.log("entering restlerHtmlFile");
+    var checks = loadChecks(checksfile).sort();
+    var generateResult = buildfn(url, checks);
+	rest.get(url).on('complete', generateResult);
+    // no return value
+    console.log("leaving restlerHtmlFile");
+}
+
+var buildfn = function(url, checks) {
+        console.log("entering buildfn with URL: %s and checks: %s",url, checks); 
+        // create a function which complies to the signature of the 
+        // rest.request call back (event "complete")
+        var generateResults = function(result, response) {
+
+            console.log("entering generateResults with result: %s and response: %s"
+                    ,result, util.format(response.message));
+            if (result instanceof Error) {
+                console.error('Error: '+ util.format(response.message));
+            } else {
+                var out = {};
+                // create a DOM from HTML
+                var $ = cheerioHtml(result);
+                console.log("returned from  cheerioHtml with $: "+ $);
+                // iterate through check-JSON
+                for(var ii in checks) {
+                    // check whether entry is present in given HTML 
+                    var present = $(checks[ii]).length > 0;
+                    console.log("* ii: %s -> present: %s",ii,present);
+                    // set the cheresult in the out-array
+                    out[checks[ii]] = present;
+                }
+                var outJson = JSON.stringify(out, null, 4);
+                console.log("OUTJSON: \n", outJson);
+            }
+        console.log("leaving generateResults");
+    };
+    console.log("leaving buildfn");
+    return generateResults;
 };
 
 var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+    console.log("### entering loadChecks");
+    var checkJson = JSON.parse(fs.readFileSync(checksfile));
+    console.log("checkJson: ", checkJson);
+    return checkJson;
 };
+/*
+var checkHtmlFile = function(url, checksfile) {
+    console.log("entering checkHtmlFile with url: %s and checksfile: %s", url, checksfile);    
+    var checks = loadChecks(checksfile).sort();
+    // load the HTML via HTTP (async)
+    restlerHtmlFile(url, checks);
+    console.log("leaving checksHtmlFile");
+}
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+	// load check-file and sort it.	
     var checks = loadChecks(checksfile).sort();
     var out = {};
+
+    // iterate through check-JSON
     for(var ii in checks) {
+        // check whether entry is present in given HTML 
         var present = $(checks[ii]).length > 0;
+        // set the cheresult in the out-array
         out[checks[ii]] = present;
     }
     return out;
 };
+
+*/
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -62,13 +130,28 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program
+    	console.log("process.argv: "+process.argv);
+    	// parse the CL parameters
+	program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        // .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to page', clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+	console.log("program.checks: " + program.checks);
+	// console.log("program.file: " + program.file);
+	console.log("program.url: " + program.url);
+    
+    restlerHtmlFile(program.url, program.checks);
+
+	// based on file
+	// var checkJson = checkHtmlFile(program.file, program.checks);
+
+	// based on url
+	// var checkJson = checkHtmlUrl(program.url, program.checks);
+    // create an JSON outfile from check-array
+	//var outJson = JSON.stringify(checkJson, null, 4);
+    //	console.log(outJson);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    	exports.checkHtmlFile = checkHtmlFile;
 }
